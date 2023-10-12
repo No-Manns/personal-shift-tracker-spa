128cf4393fa26e30ef60e15164b72ab0c6da0e88
naveenoo9 <50537003+naveenoo9@users.noreply.github.com>
2023-10-12 09:25:30 +0530

    First Commit

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpZMvXytOrpEIUk1V_EcRnxyOurMJCiAE",
+  authDomain: "personal-shift-tracker-cf9dd.firebaseapp.com",
+  projectId: "personal-shift-tracker-cf9dd",
+  storageBucket: "personal-shift-tracker-cf9dd.appspot.com",
+  messagingSenderId: "605820539184",
+  appId: "1:605820539184:web:b0ce97bc2afb420f3cb8f5",
+  measurementId: "G-S0QS7JBQ6J",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
